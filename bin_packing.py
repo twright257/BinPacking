2498ed7f380669b48e675484e5c5538cff6d5921
Tyler Wright <twright257@gmail.com>
2016-02-19 09:53:00 -0700

    bin width changes with input size

--- bin_packing.py
+++ bin_packing.py
@@ -1,28 +1,13 @@
-
 # ----------------------------------------------
 # CSCI 338, Spring 2016, Bin Packing Assignment
-# Author: John Paxton
-# Last Modified: January 25, 2016
+# Author: Tyer Wright
+# Last Modified: February 17, 2016
 # ----------------------------------------------
 # Modified to include find_naive_solution so that
 # driver does not need to be imported.  You may delete
 # find_naive_solution from your submission.
 # ----------------------------------------------
 
-"""
-FIND_NAIVE_SOLUTION:
-    Line the the top left corners of the rectangles up along
-the y = 0 axis starting with (0,0).
---------------------------------------------------
-rectangles: a list of tuples, e.g. [(w1, l1), ... (wn, ln)] where
-    w1 = width of rectangle 1,
-    l1 = length of rectangle 1, etc.
---------------------------------------------------
-RETURNS: a list of tuples that designate the top left corner placement,
-         e.g. [(x1, y1), ... (xn, yn)] where
-         x1 = top left x coordinate of rectangle 1 placement
-         y1 = top left y coordinate of rectangle 1 placement, etc.
-"""
 
 def find_naive_solution (rectangles):
     placement = []
@@ -51,12 +36,13 @@ This continues until all boxes are placed
 """
 
 def find_solution(rectangles):
-    #set bin width boundary
-    bin_width = 35000  #Have been tuning this
-    # t_width = 0
-    # for rectangle in rectangles:
-    #     t_width += rectangle[0]
-    # print(t_width)
+    #set bin width boundary based on input size
+    bin_width = 35000
+    if len(rectangles) < 2500:
+        bin_width = 25000
+    elif len(rectangles) > 7500:
+        bin_width = 45000
+
 
 
     #begin placement
@@ -79,18 +65,6 @@ def find_solution(rectangles):
 def search_levels(rectangle, level_wide, level_high, bin_width):
     y_height = 0
     #search through levels for space where rectangle fits
-    # if level_high:
-    #     #look through spaces above already placed  rectangles
-    #     for space in level_high:
-    #         if rectangle[0] <= space[0] and rectangle[1] <= space[1]:
-    #             coordinates = (space[2], space[3])
-    #             space[0] = rectangle[0]
-    #             space[1] -= rectangle[1]
-    #             space[3] -= rectangle[1]
-    #             if space[1] == 0:
-    #                 level_high.remove(space)
-    #             return coordinates
-    #look through empty level spaces
     for lev in level_wide:
         if rectangle[1] <= lev[1] and rectangle[0] <= lev[0]:   #fits within level height and leftover level width
             upper_left_x = bin_width - lev[0]
@@ -100,7 +74,7 @@ def search_levels(rectangle, level_wide, level_high, bin_width):
             if (lev[1] - rectangle[1]) > 0:
                 level_high.append([rectangle[0], lev[1] - rectangle[1], upper_left_x, upper_left_y - rectangle[1]])
             return coordinates                              #return placement coordinates
-        y_height -= lev[1]                                  #increase y placement coordinate by level height
+        y_height -= lev[1]                                  #decrease y placement coordinate by level height
     #spot not found, create new level
     if rectangle[0] <= bin_width:
         level_wide.append([(bin_width - rectangle[0]), rectangle[1]])
